cf82649625547f6915b362dd06d34b9fbd45c64f
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2023-02-05 18:09:28 +0100

    Corrigiendo 3

--- Repo1Actividad1/src/Sergio/Sergio.java
+++ Repo1Actividad1/src/Sergio/Sergio.java
@@ -3,7 +3,7 @@ package Sergio;
 public class Sergio {
 
 	
-<<<<<<< HEAD
+
 		
 		private String nombre = "Sergio";
 		private String hobbies = "Leer, ver cine, y viajar";
@@ -60,88 +60,8 @@ public String getHobbies() {
     	return "Mis hobbies son " + this.hobbies;
 		
 		
-		
-=======
-	
-	private String nombre = "Sergio";
-	private String hobbies = "Leer, ver cine, y viajar";
-	
-	
-	//Getter and Setter
+    }}	
 
-	public String getNombre() {
-		return nombre;
->>>>>>> develop
-	}
-		
-		
-
-<<<<<<< HEAD
-
-		
-		
-		
-}
-=======
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	
-	// Constructores
-
-
-public String getHobbies() {
-		return hobbies;
-	}
-
-	public void setHobbies(String hobbies) {
-		this.hobbies = hobbies;
-	}
-
-	//Constructor sin parametro
-	public Sergio() {
-		super();
-	}
-
-	
-	public Sergio(String nombre, String hobbies) {
-		super();
-		this.nombre = nombre;
-		this.hobbies = hobbies;
-	}
-
-	// to String
-	@Override
-	public String toString() {
-		return "Sergio [nombre=" + nombre + "]";
-	}
-	
-
-	
-//Método
-public String imprimirNombre() {
-	return "Hola, soy " + this.nombre;
-}	
-	
-public String imprimirHobbies() {
-	return "Mis hobbies son " + this.hobbies;
-	
-	
-	
-}
-	
-	
-
-
-	
-	
-	
-}
->>>>>>> develop
-		
-		
-		
 		
 		
 
