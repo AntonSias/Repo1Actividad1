65556ac5fe2f8ddb3c7e37bb2142737598bbcd91
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2023-02-05 17:47:05 +0100

    añadido hobbies en develop

--- Repo1Actividad1/src/Sergio/Sergio.java
+++ Repo1Actividad1/src/Sergio/Sergio.java
@@ -2,15 +2,79 @@ package Sergio;
 
 public class Sergio {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Hola, Me llamo Sergio");
-		
-		System.out.println();
-		
-		
-		
+	
+	
+	private String nombre = "Sergio";
+	private String hobbies = "Leer, ver cine, y viajar";
+	
+	
+	//Getter and Setter
 
+	public String getNombre() {
+		return nombre;
 	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	// Constructores
+
+
+public String getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(String hobbies) {
+		this.hobbies = hobbies;
+	}
+
+	//Constructor sin parametro
+	public Sergio() {
+		super();
+	}
+
+	
+	public Sergio(String nombre, String hobbies) {
+		super();
+		this.nombre = nombre;
+		this.hobbies = hobbies;
+	}
+
+	// to String
+	@Override
+	public String toString() {
+		return "Sergio [nombre=" + nombre + "]";
+	}
+	
+
+	
+//Método
+public String imprimirNombre() {
+	return "Hola, soy " + this.nombre;
+}	
+	
+public String imprimirHobbies() {
+	return "Mis hobbies son " + this.hobbies;
+	
+	
+	
 }
+	
+	
+
+
+	
+	
+	
+}
+		
+		
+		
+		
+		
+
+	
+
+
